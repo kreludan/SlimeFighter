784566b4ce75fe82ec0aa63c279b1b4915b14c1f
vikarus <vikr4m.kohli@gmail.com>
2023-07-08 00:31:38 -0500

    Remove junk

--- Assets/Scripts/Characters/Spawner.cs
+++ Assets/Scripts/Characters/Spawner.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public GameObject attackUpperRight;
     public GameObject attackUpper;
+    public GameObject attackUpperLeft;
     public GameObject attackRight;
     public GameObject attackLeft;
+    public GameObject attackLowerRight;
     public GameObject attackLower;
- 
+    public GameObject attackLowerLeft;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,28 +20,23 @@ public class Spawner : MonoBehaviour
         
     }
 
-
     public void SpawnAttack(Character.CharDirX dirX, Character.CharDirY dirY)
     {
         GameObject objectToSpawn;
-
-
         if (dirX == Character.CharDirX.LEFT)
         {
-            // objectToSpawn = (dirY == Character.CharDirY.UP) ? attackUpperLeft
-            //    : (dirY == Character.CharDirY.DOWN ? attackLowerLeft : attackLeft);
+            objectToSpawn = (dirY == Character.CharDirY.UP) ? attackUpperLeft
+                : (dirY == Character.CharDirY.DOWN ? attackLowerLeft : attackLeft);
         }
         else if (dirX == Character.CharDirX.RIGHT)
         {
-            // objectToSpawn = (dirY == Character.CharDirY.UP) ? attackUpperRight
-            //    : (dirY == Character.CharDirY.DOWN ? attackLowerRight : attackRight);
+            objectToSpawn = (dirY == Character.CharDirY.UP) ? attackUpperRight
+                : (dirY == Character.CharDirY.DOWN ? attackLowerRight : attackRight);
         }
         else
         {
             objectToSpawn = (dirY == Character.CharDirY.UP) ? attackUpper : attackLower;
         }
-
-
     }
 
 }
